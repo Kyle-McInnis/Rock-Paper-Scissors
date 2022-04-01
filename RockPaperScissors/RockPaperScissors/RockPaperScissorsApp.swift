db3d86748fd7188aa53bb44731dab25189dc3883
Kyle McInnis <kyle.ap.mcinnis@gmail.com>
2022-04-01 07:13:15 -0300

    Adding rock-paper-scissors app to git repo

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Kyle McInnis on 2022-02-17.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
